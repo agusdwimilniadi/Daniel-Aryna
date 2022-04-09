79accd00d10cc873ab83ee54c2ad25b7ae645c87
Agus Dwi <agusdwimill@gmail.com>
2022-04-10 01:50:32 +0700

    add header undangan

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import Section1 from "./Page/Section1";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Section1 />
+      <Routes>
+        <Route path="/" element={<Section1 />} />
+        <Route path="/:undangan" element={<Section1 />} />
+      </Routes>
     </>
   );
 }
--- src/Page/Section1.js
+++ src/Page/Section1.js
@@ -1,8 +1,60 @@
 import React from "react";
-// import bgImage from "../image/#PW00001 (15).jpg";
+import { useParams } from "react-router-dom";
+import $ from "jquery";
 export default function Section1() {
+  function play() {
+    var audio = document.getElementById("audio");
+    audio.play();
+  }
+
+  const fungsiOpen = () => {
+    $(".section-0").animate(
+      { textIndent: 0 },
+      {
+        step: function () {
+          $(this).css("transform", "translateY(200%)");
+        },
+        duration: "slow",
+      },
+      "linear"
+    );
+    play();
+  };
+  const params = useParams();
+
   return (
     <>
+      <section
+        className="section-0 "
+        style={{
+          backgroundImage: `url('image/1.jpeg')`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center center",
+          backgroundBlendMode: "overlay",
+        }}
+      >
+        <div className="container-fluid container-auto">
+          <div className="text-center">
+            <h3 className="pt-5 heading-1-0">Hello</h3>
+            <div className="pt-2 name-1 text-white " onClick={play}>
+              <h1>{params === "" ? "Guys" : params.undangan}</h1>
+            </div>
+            <div className="pt-4 ">
+              <h6 className="date">You're Invited</h6>
+              <h6 className="date">19 / MEI / 2022</h6>
+            </div>
+            <div className="my-4">
+              <button
+                onClick={fungsiOpen}
+                className="btn btn-primary-custom font-primary text-white"
+              >
+                Open Invitation
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
       <section
         className="section-1 d-flex align-items-center"
         style={{
@@ -13,11 +65,13 @@ export default function Section1() {
           backgroundBlendMode: "overlay",
         }}
       >
+        <audio src="music/music.mp3" id="audio" autoPlay></audio>
         <div className="container-fluid ">
           <div className="text-center">
             <h5 className="pt-5 heading-1">
               WE INVITE YOU TO CELEBRATE OUR WEEDING
             </h5>
+
             <div className="pt-3">
               <img
                 src="https://acarakami.com/wp-content/uploads/2020/10/icon-leaf-acarakami.svg"
@@ -30,7 +84,7 @@ export default function Section1() {
                 THE WEEDING OF
               </p>
             </div>
-            <div className="pt-2 name-1 text-white ">
+            <div className="pt-2 name-1 text-white " onClick={play}>
               <h1>
                 Daniel <br /> & <br /> Aryna
               </h1>
